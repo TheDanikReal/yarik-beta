f12343f622a0b0a233348bb2798eef04d37f5c1c
TheDanikReal <99135042+TheDanikReal@users.noreply.github.com>
2025-06-20 09:40:39 +0000

    Fix docker container

--- build.ts
+++ build.ts
@@ -3,15 +3,8 @@ import { readFile, writeFile } from "node:fs/promises"
 
 const fileName = "bundle.cjs"
 
-/*const urlPlugin: Plugin = {
-    name: "urlPlugin",
-    setup(build) {
-        
-    },
-}*/
-
 await build({
-    entryPoints: ["index.ts"],
+    entryPoints: ["api.ts"],
     bundle: true,
     outfile: fileName,
     platform: "node"
@@ -20,6 +13,8 @@ await build({
 
 const regex = /import_meta[1-9]*\.url/g
 
+// new version of prisma client uses import.meta.url for finding query engine, which is
+// not supported on cjs, so we are replacing it with file:///
 let file = await readFile(fileName, "utf-8")
 file = file.replaceAll(regex, "\"file:///\"")
 writeFile(fileName, file, "utf-8")
