14b7cb444a4365d7a96cecf4142f97d731dbf1d9
TheDanikReal <99135042+TheDanikReal@users.noreply.github.com>
2025-07-16 18:19:09 +0000

    Add caching for api.ts

--- api.ts
+++ api.ts
@@ -3,6 +3,7 @@ import { settings } from "./settings.ts"
 import { bot, logger, startBot } from "./index.ts"
 import console from "node:console"
 import process from "node:process"
+import { setTimeout } from "node:timers"
 
 interface DiscordStatus {
     page: {
@@ -25,26 +26,38 @@ async function fetchDiscordStatus() {
     return
 }
 
+async function cacheMessage() {
+    if (discordStatusRefreshed <= Date.now() - 1000 * 60 * settings.statusCooldown) {
+        await fetchDiscordStatus()
+        console.log("refreshed status")
+        discordStatusRefreshed = Date.now()
+    }
+    cachedMessage = `{ "status": ${bot.isReady()}, "discordStatus": ${discordStatus} }`
+    cached = true
+    setTimeout(() => {
+        cached = false
+    }, 5000);
+}
+
 startBot()
 
 logger.info("started")
 
 const port = process.env.PORT || 3000
 const url = process.env.STATUS_URL || "https://discordstatus.com/api/v2/status.json"
+let cached = false
+let cachedMessage = ""
 let discordStatusRefreshed = Date.now()
 let discordStatus = false
 fetchDiscordStatus()
 
 const server = http.createServer(async (req, res) => {
-    if (discordStatusRefreshed <= Date.now() - 1000 * 60 * settings.statusCooldown) {
-        await fetchDiscordStatus()
-        console.log("refreshed status")
-        discordStatusRefreshed = Date.now()
-    }
-    if (req.url == "/") {
+    if (req.url === "/") {
         res.writeHead(200, { "content-type": "application/json" })
-        res.end(`{ status: ${bot.isReady()}, discordStatus: ${discordStatus} }`)
+        if (!cached) await cacheMessage()
+        res.end(cachedMessage)
     }
+    return
 })
 
 server.listen(port, () => {
